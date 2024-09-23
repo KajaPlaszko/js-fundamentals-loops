2b713b0cf684231c7993531213f21683377418b9
Kaja Thoresen Plaszko <kaja.plaszko@no.experis.com>
2024-09-23 12:19:02 +0200

    Kaja Plaszko

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -1,25 +1,63 @@
 const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-line no-unused-vars
 
-// 1. Use a for loop to set the variable hasTen to true if the array contains the value 10
-// note: use a break statement to exit the loop early if the value is found
-// to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
+// 1. Check if the array contains the value 10
 let hasTen = false
 let indexOfTen = -1
 
-// 2. Use a for loop to count how many numbers in the array are divisible by 3
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
+
+// 2. Count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
 
-// 3. use a for loop to find the average of the numbers in the array
-let average = 0
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
 
-// 4. use a for loop to find the largest number in the array
-let largest = 0
+// 3. Find the average of the numbers in the array
+let sum = 0
 
-// 5. use a for loop to find the smallest number in the array
-let smallest = 100000
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+}
 
-// 6. find the median of the numbers in the array
-let median = 0
+const average = sum / nums.length
+
+// 4. Find the largest number in the array
+let largest = nums[0]
+
+for (let i = 1; i < nums.length; i++) {
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
+
+// 5. Find the smallest number in the array
+let smallest = nums[0]
+
+for (let i = 1; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
+
+// 6. Find the median of the numbers in the array
+const sortedNums = nums.slice().sort((a, b) => a - b)
+const midIndex = Math.floor(sortedNums.length / 2)
+
+let median
+if (sortedNums.length % 2 === 0) {
+  median = (sortedNums[midIndex - 1] + sortedNums[midIndex]) / 2
+} else {
+  median = sortedNums[midIndex]
+}
 
 module.exports = {
   hasTen,
